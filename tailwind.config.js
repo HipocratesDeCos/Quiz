9d64317a4bcbc2064a7186b84745c8027fa0e2eb
HipocratesDeCos <andresragel2024@gmail.com>
2025-08-19 22:35:11 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3a86ff',
+        secondary: '#8338ec',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
